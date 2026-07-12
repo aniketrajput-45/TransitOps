e7201a99e47bb3c3b05a4eb6af71eede997cf312
aniketrajput-45 <rajputaniket7234@gmail.com>
2026-07-12 12:14:39 +0530

    initial commit

---
+++ transit-ops/backend/src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Health Check Route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "🚀 TransitOps Backend Running Successfully"
+  });
+});
+
+export default app;
